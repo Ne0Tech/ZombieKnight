43bada6b9421f191d920ebc48fb33b5b46970a0c
Ne0Tech <pedigzav001@warren.k12.in.us>
2025-05-23 13:08:46 -0400

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -3,29 +3,36 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     """A class to represent a 32x32 pixel area in our display"""
 
-    def __init__(self, x, y, image_int, main_group, sub_group=""):
+    def __init__(self, x, y, image_int, main_group, sub_group=None):
         super().__init__()
-        #Load in the correct image and add it to the correct sub group
-        #Dirt tiles
+
         if image_int == 1:
-            self.image = pygame.transform.scale(pygame.image.load("./assets/images/tiles/Tile (1).png"), (32, 32))
-        #Platform tiles
+            self.image = pygame.transform.scale(
+                pygame.image.load("./assets/images/tiles/Tile (1).png"), (32, 32)
+            )
         elif image_int == 2:
-            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (2).png
-            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
-            pass # TODO: remove this when done.
+            self.image = pygame.transform.scale(
+                pygame.image.load("./assets/images/tiles/Tile (2).png"), (32, 32)
+            )
+            sub_group.add(self)
         elif image_int == 3:
-            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (3).png
-            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
-            pass # TODO: remove this when done.
+            self.image = pygame.transform.scale(
+                pygame.image.load("./assets/images/tiles/Tile (3).png"), (32, 32)
+            )
+            sub_group.add(self)
         elif image_int == 4:
-            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (4).png
-            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
-            pass # TODO: remove this when done.
+            self.image = pygame.transform.scale(
+                pygame.image.load("./assets/images/tiles/Tile (4).png"), (32, 32)
+            )
+            sub_group.add(self)
+
         elif image_int == 5:
-            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (5).png
-            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
-            pass # TODO: remove this when done.
+            self.image = pygame.transform.scale(
+                pygame.image.load("./assets/images/tiles/Tile (5).png"), (32, 32)
+            )
+            sub_group.add(self)
+
+
 
         #Add every tile to the main group
         main_group.add(self)
